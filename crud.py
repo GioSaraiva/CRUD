8781b0679dfaa9ce41a991be6c0b621f1600ccf3
GioSaraiva <142528977+GioSaraiva@users.noreply.github.com>
2023-11-19 22:51:52 -0300

    Add files via upload

--- crud.py
+++ crud.py
@@ -1,61 +1,40 @@
 import mysql.connector
-from datetime import datetime
+from biblioteca import*
 
-"""Dados de entrada:alunos, modalidade, personal, funcionarios
-Menu: inserir atualizar, deletar, visualizar. escolher a tabela que será feita a alteração"""
-import mysql.connector
-from bliblioteca_banco_dados import *
+conexao = mysql.connector.connect(host="localhost", user="root", password="302302", database="salao")
 
-# Conectar ao banco de dados
+entrada = input(f'Aperte f-funcionarios / c-clientes / m-materiais / s-serviços')
 
+if entrada == 'f':
+    at1 = str(input('Digite o nome do funcionario: '))
+    at2 = float(input('Digite o salário do funcionario: '))
+    at3 = str(input('Digite a data de nascimento:    EX.:(**/**/****)'))
+    at4 = '-------------------------------------------------------------'
+    criar(conexao, entrada, at1, at2, at3, at4)
+    ler(conexao,'funcionarios')
+    deletar(conexao, tabela)
 
-while True:
-    conexao = mysql.connector.connect(
-        host="localhost", user="root", password="root", database="supermercado"
-    )
-    tabela = input("Qual tabela ira usar?1-produtos 2-funcionarios 3-setores 4-sair")
+elif entrada == 'c':
+    at1 = str(input('Digite o nome do cliente: '))
+    at2 = str(input('Digite a data de nascimento do cliente: EX.:(**/**/****) '))
+    at3 = str(input('Digite o endereço do cliente: '))
+    at4 = int(input('Digite o telefone do cliente: '))
+    criar(conexao, entrada, at1, at2, at3, at4)
+    ler(conexao, 'clientes')
 
-    if tabela == "3":
-        escolha = input("digite a-atualizar d-deletar, v-visualizar")
+if entrada == 'm':
+    at1 = str(input('Digite o nome do material: '))
+    at2 = str(input('Digite a data de validade: EX.:(**/**/****) '))
+    at3 = int(input('Digite a quantidade: '))
+    at4 = '---------------------------------------------------------------'
+    criar(conexao, entrada, at1, at2, at3, at4)
+    ler(conexao, 'materiais')
 
-        v1 = input("Qual o nome do setor ira ser cadastrado?")
-        criar(conexao, tabela, v1)
+if entrada == 's':
+    at1 = str(input('Digite o nome do serviço: '))
+    at2 = str(input('Digite o tempo do serviço: EX.:(1h30) '))
+    at3 = int(input('Digite o valor do serivço:'))
+    at4 = '---------------------------------------------------------------'
+    criar(conexao, entrada, at1, at2, at3, at4)
+    ler(conexao, 'serviços')
 
-        if escolha == "v":
-            leia = input("o que deseja ler?")
-
-            ler(conexao, leia)
-        elif escolha == "d":
-            tabela_s = input("selecione qual a tabela ira apagar")
-            id = int(input("id da tabela"))
-            deletar(conexao,tabela, id)
-        elif escolha == "a":
-            tabela = input("qual tabela deseja alterar?")
-            id = int(input("Qual o id da tabela?"))
-            coluna = input("Selecione a coluna")
-            novo_valor = input("Novo valor")
-            atualizar(conexao,tabela,id,coluna,novo_valor)
-
-
-
-
-    elif tabela == "4":
-        break
-    elif tabela == "2":
-        leia = "funcionarios"
-        v1 = input("Qual o nome do funcionario?")
-        v2 = float(input("Qual o salario  ira ser cadastrado?"))
-        criar(conexao, tabela, v1, v2)
-        ler(conexao, leia)
-
-    else:
-        leia = "produtos"
-        v1 = input("Qual o nome do produto?")
-        v2 = str(input("Qual a data de validade?Use o formato YYYY-MM-DD"))
-        v3 = float(input("Qual o preço cadastrado?"))
-
-        criar(conexao, tabela, v1, v2, v3)
-        ler(conexao, leia)
-
-
-conexao.close()
